c2aa30624c8b87997c762b725abd0efd25194ba1
Kevin CT <kcayo6155@uta.edu.ec>
2023-08-08 11:21:51 -0500

    feat: Agrega nueva pagina

---
+++ projects/app-gsv/src/app/borrar/borrar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BorrarRoutingModule } from './borrar-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BorrarRoutingModule
+  ]
+})
+export class BorrarModule { }
